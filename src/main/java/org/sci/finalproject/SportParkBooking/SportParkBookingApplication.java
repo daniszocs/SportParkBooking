f09077948479d6a6c25c6da6da0c4b4e2444fdf6
daniel.szocs <szocsdani@gmail.com>
2021-04-30 09:50:41 +0300

    2021 04 30 9:50

---
+++ src/main/java/org/sci/finalproject/SportParkBooking/SportParkBookingApplication.java
@@ -0,0 +1,13 @@
+package org.sci.finalproject.SportParkBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportParkBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportParkBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sci/finalproject/SportParkBooking/SportParkBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sci.finalproject.SportParkBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportParkBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
