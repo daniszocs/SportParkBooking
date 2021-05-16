b8db356c784f05110504263ce3a433f5d44b164b
daniel.szocs <szocsdani@gmail.com>
2021-05-16 22:31:53 +0300

    added /selectPlayGround?sportName=Football or Tennis sportName is optional

--- src/main/java/org/sci/finalproject/SportParkBooking/controller/PlayGroundController.java
+++ src/main/java/org/sci/finalproject/SportParkBooking/controller/PlayGroundController.java
@@ -1,13 +1,17 @@
 package org.sci.finalproject.SportParkBooking.controller;
 
 import org.sci.finalproject.SportParkBooking.model.PlayGround;
+import org.sci.finalproject.SportParkBooking.model.Sport;
 import org.sci.finalproject.SportParkBooking.service.PlayGroundService;
 import org.sci.finalproject.SportParkBooking.service.SportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -18,7 +22,7 @@ public class PlayGroundController {
     private SportService sportService;
 
     @GetMapping({"/selectPlayGround"})
-    public String selectPlayGround(Model model) {
+    public String selectPlayGround(Model model, @RequestParam(value="sportName", required=false) String sportName) {
 
         //************************************************************************
 
@@ -43,7 +47,23 @@ public class PlayGroundController {
 //        playGroundList.add(playGround3);
 
         Iterable<PlayGround> iterable = playGroundService.findAll();
-        iterable.forEach(playGroundList::add);
+        Iterator<PlayGround> iterator = iterable.iterator();
+
+        while(iterator.hasNext()) {
+            PlayGround element = iterator.next();
+//            sportName = "Football";
+            if (sportName==null) {
+                System.out.println("HERE_IF");
+                playGroundList.add(element);
+            }
+            else{
+                System.out.println("HERE_ELSE");
+                Long sportId = sportService.returnSportId(sportName);
+                if (element.getSportId()==sportId) {
+                    playGroundList.add(element);
+                }
+            }
+        }
 
         model.addAttribute("myPlayGroundList", playGroundList);
         return "selectPlayGround";
