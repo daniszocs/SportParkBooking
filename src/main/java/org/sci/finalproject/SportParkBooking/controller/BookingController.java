4648108d8b9482eed473cc45a29ba3f24a2e5881
daniel.szocs <szocsdani@gmail.com>
2021-05-19 20:27:07 +0300

    select Booking + confirm Booking (partial)

--- src/main/java/org/sci/finalproject/SportParkBooking/controller/BookingController.java
+++ src/main/java/org/sci/finalproject/SportParkBooking/controller/BookingController.java
@@ -1,4 +1,48 @@
 package org.sci.finalproject.SportParkBooking.controller;
 
+import org.sci.finalproject.SportParkBooking.model.Booking;
+import org.sci.finalproject.SportParkBooking.service.PlayGroundService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class BookingController {
+    @Autowired
+    private PlayGroundService playGroundService;
+
+
+    @RequestMapping("/selectBooking")
+    public String mySelectBookingPage(Model model) {
+        Booking emptyBooking = new Booking();
+        model.addAttribute("booking", emptyBooking);
+        return "selectBooking";
+    }
+
+//
+//
+//    @RequestMapping("/register")
+//    public String myRegisterPage(Model model) {
+//        User emptyUser = new User();
+//        model.addAttribute("user", emptyUser);
+//        return "register";
+//    }
+//
+//    @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
+//    public String registerUser(@ModelAttribute("user") User user, BindingResult errors, Model model) {
+//        boolean registerResult = userService.register(user);
+//        if (registerResult) {
+//            return confirmRegister(model, user.getUserName());
+//        } else {
+//            return "error";
+//        }
+//    }
+//
+//    @GetMapping({"/confirmRegister"})
+//    public String confirmRegister(Model model, @RequestParam(value="name", required=false) String name) {
+//        model.addAttribute("name", name);
+//        return "confirmRegistration";
+//    }
 }
